1e0bc2bde301e915c3cdaa2efb6f8444e929c3a4
Boyiddhanath Roy <boyiddha.hstu.ece@gmail.com>
2024-08-26 11:47:34 +0600

    upload files

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StoryTelling Platform</title>
+    <link rel="stylesheet"type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <header class="header" >
+        <h1>Interactive Storytelling Platform</h1><br><br>
+        <nav class="navigation">
+            <ul>
+                <li><a target="" href="read_story.php">Read Story</a></li>
+                <li><a target="" href="create_story.php">Create Story</a></li
+            </ul>
+        </nav>
+    </header><br>
+	
+    <div class="content">
+		<h1>Interactive Story?</h1><br>
+		
+        <p>Interactive storytelling, also known as interactive storytelling, is a form of storytelling where readers 
+		are given the opportunity to interact with the story and shape its outcome. It is often done through a combination 
+		of text and images, and sometimes audio and video, to create a gripping and engaging narrative.
+		<br><br>
+
+		Rather than being limited to a linear narrative, readers can choose from a variety of options to determine 
+		where the story will go. This means that every reader can have a unique experience and create their own story. 
+		It also means that readers can go back and explore different paths and endings.
+		<br><br>
+
+		An interactive story can be used to create stories with multiple endings or stories that evolve over time 
+		and change based on the reader's preferences. It's a great way to engage readers and get them invested in the story.
+		</p>
+    </div>
+
+    <footer class="footer">
+        <p>© 2024 Boyiddhanath Roy. All rights reserved.</p>
+    </footer>
+</body>
+
+</html>
